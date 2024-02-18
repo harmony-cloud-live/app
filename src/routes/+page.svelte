fd488fc72d5ada0e8104d9701d0b0b4674b4137e
kate-jiang <katejiang@pm.me>
2024-02-18 00:54:57 -0500

    flash on leader change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,75 @@
-<script lang='ts'>
+<script lang="ts">
     import {
         ChordButtons,
         ChordDisplay,
         Player,
         ManualButtons,
-        ManualPlayer
-    } from '$lib/components';
-    import { aiMode } from '$lib/stores';
-    import { cubicInOut } from 'svelte/easing';
-    import { fly } from 'svelte/transition';
+        ManualPlayer,
+    } from "$lib/components";
+
+    import { aiMode, isLeader } from "$lib/stores";
+    import { onMount } from "svelte";
+    import { cubicInOut } from "svelte/easing";
+    import { fly } from "svelte/transition";
+
+    let flash = false;
+
+    const unsub = isLeader.subscribe((val) => {
+        if (val) {
+            console.log('hi')
+            flash = true;
+            setTimeout(() => {
+                flash = false;
+            }, 1000);
+        }
+    });
+
+    onMount(() => {
+        return () => {
+            unsub();
+        };
+    });
 </script>
 
 {#if $aiMode}
-    <section transition:fly={{x: -2500, duration: 300, easing: cubicInOut}}>
+    <section transition:fly={{ x: -2500, duration: 300, easing: cubicInOut }}>
         <ChordDisplay />
         <ChordButtons />
         <Player />
     </section>
 {:else}
-    <section transition:fly={{x: 2500, duration: 300, easing: cubicInOut}}>
+    <section transition:fly={{ x: 2500, duration: 300, easing: cubicInOut }}>
         <ManualButtons />
         <ManualPlayer />
     </section>
 {/if}
 
+
+{#if flash}
+    <div class="flash"></div>
+{/if} 
+
 <style>
     section {
         position: absolute;
+    }
+
+    .flash {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: white;
+        animation: flashAnimation 1s forwards;
+    }
+
+    @keyframes flashAnimation {
+        from {
+            opacity: .2;
+        }
+        to {
+            opacity: 0;
+        }
     }
 </style>
